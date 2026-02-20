d08394da5753722aa6d82d015552a2b23c4c0e58
Miguel Reis <ffreis8764@gmail.com>
2026-02-20 09:19:33 -0300

    Usando plt

--- dados-python-inicio2/main.py
+++ dados-python-inicio2/main.py
@@ -3,6 +3,13 @@ from random import choice, randrange, sample
 import numpy as np
 # Exercicios inicias
 
+def treino1():
+    estudantes = ["João", "Maria", "José"]
+    notas = [8.5, 9, 6.5]
+    plt.bar(x = estudantes, height = notas)
+    plt.show()
+
+
 
 
 
