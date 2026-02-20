43ade7fc3374315889d3e45b7f160ce5c2bc011d
Miguel Reis <ffreis8764@gmail.com>
2026-02-20 09:38:01 -0300

    Usando biblioteca math

--- dados-python-inicio2/main.py
+++ dados-python-inicio2/main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt   
 from random import choice, randrange, sample
 import numpy as np
+from math import pow
 # Exercicios inicias
 
 # Conhecendo plt
@@ -27,8 +28,20 @@ def treino2():
 # Treinando funções
 def treino3():
     lista = [8, 12, 54, 23, 43, 1, 90, 87, 105, 77]
-        print(f"Dada a lista: {lista}")
+    print(f"Dada a lista: {lista}")
     numero = choice(lista)
     print(f"O numero sorteado foi: {numero}")
 
-treino3()
+def treino4():
+    print("Gerando numero positivo aleatorio de 1 a 100...")
+    numero = randrange(100)
+    print(f"Numero gerado! O numero é {numero}.")
+
+def treino5():
+    print("Por favor, digite dois numero a seguir para calcularmos a potência.")
+    numero1 = int(input("Digite o primero numero: "))
+    numero2 = int(input("Digite o segundo numero: "))
+    result = pow(numero1, numero2)
+    print(result)
+
+treino5()
