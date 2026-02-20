50997a3ffdcf218b141058a48b4b2e225b50a7f8
Miguel Reis <ffreis8764@gmail.com>
2026-02-20 09:22:28 -0300

    Exercicio alura treino

--- dados-python-inicio2/main.py
+++ dados-python-inicio2/main.py
@@ -3,12 +3,14 @@ from random import choice, randrange, sample
 import numpy as np
 # Exercicios inicias
 
+# Conhecendo plt
 def treino1():
     estudantes = ["João", "Maria", "José"]
     notas = [8.5, 9, 6.5]
     plt.bar(x = estudantes, height = notas)
     plt.show()
 
+# Conhecendo Random e funções dele
 def treino2():
     estudantes = ["João", "Maria", "José", "Ana"]
 
@@ -22,5 +24,11 @@ def treino2():
     smp = sample(lista, 3)  
     print(smp)  
 
+# Treinando funções
+def treino3():
+    lista = [8, 12, 54, 23, 43, 1, 90, 87, 105, 77]
+        print(f"Dada a lista: {lista}")
+    numero = choice(lista)
+    print(f"O numero sorteado foi: {numero}")
 
-
+treino3()
