432b0388f0992ffeb48e0f33b1cf0c00a382ba5c
Miguel Reis <ffreis8764@gmail.com>
2026-02-20 21:16:42 -0300

    Exercicio sortear ate o numero q o usuario digita

--- dados-python-inicio2/main.py
+++ dados-python-inicio2/main.py
@@ -4,12 +4,14 @@ import numpy as np
 from math import pow
 # Exercicios inicias
 
+
 # Conhecendo plt
 def treino1():
     estudantes = ["João", "Maria", "José"]
     notas = [8.5, 9, 6.5]
     plt.bar(x = estudantes, height = notas)
     plt.show()
+
 
 # Conhecendo Random e funções dele
 def treino2():
@@ -25,6 +27,7 @@ def treino2():
     smp = sample(lista, 3)  
     print(smp)  
 
+
 # Treinando funções
 def treino3():
     lista = [8, 12, 54, 23, 43, 1, 90, 87, 105, 77]
@@ -32,16 +35,36 @@ def treino3():
     numero = choice(lista)
     print(f"O numero sorteado foi: {numero}")
 
+
 def treino4():
     print("Gerando numero positivo aleatorio de 1 a 100...")
     numero = randrange(100)
     print(f"Numero gerado! O numero é {numero}.")
 
+
 def treino5():
     print("Por favor, digite dois numero a seguir para calcularmos a potência.")
     numero1 = int(input("Digite o primero numero: "))
     numero2 = int(input("Digite o segundo numero: "))
-    result = pow(numero1, numero2)
+    result = (numero1 ** numero2)
     print(result)
 
-treino5()
+#Um programa deve ser escrito para sortear uma pessoa seguidora de uma 
+# rede social para ganhar um prêmio. A lista de participantes é numerada e 
+# devemos escolher aleatoriamente um número de acordo com a quantidade de participantes. 
+# Peça à pessoa usuária para fornecer o número de participantes do sorteio e devolva 
+# para ela o número sorteado.
+def treino6():
+    print("VAMOS REALIZAR UM SORTEIO!")
+
+    try:
+        quantidade = int(input("Digite a quantidade de participantes: "))
+        lista = list(range(1, quantidade + 1))
+
+        sorteado = choice(lista)
+        print(f"Número sorteado: {sorteado}")
+
+    except ValueError:
+        print("Digite apenas números!")
+
+treino6()
