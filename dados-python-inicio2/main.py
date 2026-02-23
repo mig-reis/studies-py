ad05a356444a24b153ebf8f0f263a35fb9fee70e
Miguel Reis <ffreis8764@gmail.com>
2026-02-23 08:51:08 -0300

    study: valor do m2

--- dados-python-inicio2/main.py
+++ dados-python-inicio2/main.py
@@ -116,5 +116,22 @@ def treino8():
 
         if raiz == int(raiz):
             print(f"A raiz quadrada de {numero} é inteira: {int(raiz)}")
+
+
+# Faça um programa para uma loja que vende grama para jardins. Essa loja 
+# trabalha com jardins circulares e o preço do metro quadrado da grama 
+# é de R$ 25,00. Peça à pessoa usuária o raio da área circular e devolva 
+# o valor em reais do quanto precisará pagar.
+def treino9():
+    try:
+        raio = float(input("Digite o raio da aréa que você deseja adquirir a grama: "))
+
+        area = math.pi * raio ** 2
+        valor_final = area * 25
+
+        print(f"O jardim de {area:.2f}m² custará R$ {valor_final:.2f}")
+
+    except ValueError:
+        print("Por favor, digite um NUMERO.")
     
-treino8()
+treino9()
