d6b710ea403cea7beb2ea9e8957ef7668eb4d143
Miguel Reis <ffreis8764@gmail.com>
2026-02-24 09:15:32 -0300

    study: treinando map e lambda

--- dados-python-inicio2/main.py
+++ dados-python-inicio2/main.py
@@ -218,4 +218,12 @@ def treino14():
 
     print(mult_3)
 
-treino14()
+
+# Crie uma lista dos quadrados dos números da seguinte lista . Lembre-se de utilizar as funções lambda e map() para calcular o quadrado de cada elemento da lista.
+def treino15():
+    lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    lista_atualizada = list(map(lambda x: x**2, lista))
+    print(lista_atualizada)
+
+treino15()
