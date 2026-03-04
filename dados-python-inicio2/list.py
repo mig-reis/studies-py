18b25fae1e2cfa57c62047b50b30f28059cb0e62
Miguel Reis <ffreis8764@gmail.com>
2026-03-04 17:00:22 -0300

    study: list-compression usando enumerate

--- dados-python-inicio2/list.py
+++ dados-python-inicio2/list.py
@@ -12,6 +12,16 @@ def terceiro_elemento(lista):
     return  ["Peso ok" if n[-1] <= 80 else "Acima do peso" for n in lista]
 
 
-lista_de_tuplas = [('Pedro', 1.74, 81), ('Júlia', 1.65, 67), ('Otávio', 1.81, 83)]
-elemento3 = terceiro_elemento(lista_de_tuplas)
-print([i[0] for i in lista_de_tuplas], "\n", elemento3)
+    lista_de_tuplas = [('Pedro', 1.74, 81), ('Júlia', 1.65, 67), ('Otávio', 1.81, 83)]
+    elemento3 = terceiro_elemento(lista_de_tuplas)
+    print([i[0] for i in lista_de_tuplas], "\n", elemento3)
+
+def nome_organizacao(lista):
+    #A partir da lista, crie um código para gerar uma lista de tuplas em que cada tupla tenha o primeiro elemento como a posição do nome na lista original e o segundo elemento sendo o próprio nome
+
+    # O enumerate entrega o índice (i) e o valor (v) de cada item
+    return [(i+1, v) for i, v in enumerate(lista)]
+
+lista = ['Pedro', 'Júlia', 'Otávio', 'Eduardo']
+nome = nome_organizacao(lista)
+print(nome)
