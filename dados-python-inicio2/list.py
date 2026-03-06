d1a8d8d116e4739e9d87aa25de343073b10acfc9
Miguel Reis <ffreis8764@gmail.com>
2026-03-06 12:02:09 -0300

    Study: list compression dicionario estados

--- dados-python-inicio2/list.py
+++ dados-python-inicio2/list.py
@@ -146,17 +146,36 @@ def relatorio(list1, list2, list3):
     for produto, qtd, preco in zip(list1, list2, list3)
     ]
 
-produtos = ["Notebook", "Mouse", "Teclado", "Monitor", "Headset"]
-quantidades = [5, 10, 7, 3, 8]
-precos = [3500.0, 80.0, 150.0, 900.0, 200.0]
+    produtos = ["Notebook", "Mouse", "Teclado", "Monitor", "Headset"]
+    quantidades = [5, 10, 7, 3, 8]
+    precos = [3500.0, 80.0, 150.0, 900.0, 200.0]
 
-df = pd.DataFrame({
-    "produto" : produtos,
-    "quantidade": quantidades,
-    "Preços": precos
-})
+    #df = pd.DataFrame({
+        #"produto" : produtos,
+        #"quantidade": quantidades,
+        #"Preços": precos
+        #})
 
-df["Faturamento"] = df["quantidade"] * df["Preços"]
-df["Status"] = df["Faturamento"].apply(lambda x: "Baixa venda" if x < 1000 else ("Média Venda" if x < 5000 else "Alta venda"))
+        #df["Faturamento"] = df["quantidade"] * df["Preços"]
+        #df["Status"] = df["Faturamento"].apply(lambda x: "Baixa venda" if x < 1000 else ("Média Venda" if x < 5000 else "Alta venda"))
 
-print(df)
+        #print(df)
+
+
+def filiais(estados):
+    
+    # Uma empresa possui filiais espalhadas nos Estados da região Sudeste do Brasil. Em uma das tabelas de cadastro das filiais há uma coluna contendo a informação de qual é o Estado a que pertence:
+
+    # A empresa sempre está abrindo novas filiais, de modo que a tabela está constantemente recebendo novos registros e o gestor gostaria de possuir a informação atualizada da quantidade de filiais em cada Estado.
+
+    #A partir da coluna com a informação dos Estados, crie um dicionário usando dict comprehension com a chave sendo o nome de um Estado e o valor sendo a contagem de vezes em que o Estado aparece na lista.
+
+    return {estado: estados.count(estado) for estado in set(estados)}
+
+estados = ['SP', 'ES', 'MG', 'MG', 'SP', 'MG', 'ES', 'ES', 'ES', 'SP', 'SP', 'MG', 'ES', 'SP', 'RJ', 'MG', 'RJ', 'SP', 'MG', 'SP', 'ES', 'SP', 'MG']
+
+contadorsp, contadorrj, contadores, contadormg = filiais(estados)
+
+ok = filiais(estados)
+print(ok)
+
